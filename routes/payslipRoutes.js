1e7e93a1c628bfede97337078dc373d7b2d24030
ashley-chiteri <ochamiashley97@gmail.com>
2025-09-02 18:05:28 +0300

    Implemented the email payslip function

--- routes/payslipRoutes.js
+++ routes/payslipRoutes.js
@@ -1,7 +1,7 @@
 // backend/routes/payslipRoutes.js
 
 import express from 'express';
-import { generatePayslipPdf } from '../controllers/payslipController.js';
+import { generatePayslipPdf, emailPayslip } from '../controllers/payslipController.js';
 import verifyToken from '../middleware/auth.js';
 
 const router = express.Router({ mergeParams: true });
@@ -10,5 +10,7 @@ const router = express.Router({ mergeParams: true });
 // Define the route to download the payslip
 // The :companyId is included for context and security
 router.get('/:payrollDetailId/download', verifyToken, generatePayslipPdf);
+// Define the route to email the payslip
+router.post('/:payrollDetailId/email', verifyToken, emailPayslip);
 
 export default router;
